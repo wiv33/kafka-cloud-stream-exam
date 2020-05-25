8eccebf5c5b8077ec673556595cfcabbe317c04d
PSK <wiv821253@gmail.com>
2020-05-25 23:05:22 +0900

    add count

--- src/main/java/com/psawesome/kafkacloudstreamexam/KafkaCloudStreamExamApplication.java
+++ src/main/java/com/psawesome/kafkacloudstreamexam/KafkaCloudStreamExamApplication.java
@@ -1,27 +1,40 @@
 package com.psawesome.kafkacloudstreamexam;
 
-import io.netty.util.concurrent.FastThreadLocalThread;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
+import org.apache.kafka.streams.state.KeyValueIterator;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
+import org.apache.kafka.streams.state.internals.QueryableStoreProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Random;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
@@ -57,6 +70,7 @@ public class KafkaCloudStreamExamApplication {
   @Component
   @RequiredArgsConstructor
   public static class PageViewEventSource implements ApplicationRunner {
+
     private final MessageChannel pageViewOut;
 
     @Override
@@ -84,15 +98,106 @@ public class KafkaCloudStreamExamApplication {
       Executors.newScheduledThreadPool(1).scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
     }
   }
+/*
+
+  @Slf4j
+  @Component
+  public static class PageViewEventSink {
+
+    @StreamListener
+    @SendTo(AnalyticsBinding.PAGE_COUNT_OUT)
+    public KStream<String, Long> process(@Input(AnalyticsBinding.PAGE_VIEWS_IN) KStream<String, PageViewEvent> events) {
+
+      KTable<String, Long> kTable = events.filter(((key, value) -> value.getDuration() > 10))
+              .map((key, value) -> new KeyValue<>(value.getPage(), "0"))
+              .groupByKey()
+//              .windowedBy(TimeWindows.of(Duration.ofMinutes(1)))
+              .count(Materialized.as(AnalyticsBinding.PAGE_VIEW_MV));
+
+      events.leftJoin(kTable, new ValueJoiner<PageViewEvent, Long, LocalDateTime>() {
+        @Override
+        public LocalDateTime apply(PageViewEvent value1, Long value2) {
+          return null;
+        }
+      });
+    }
+      return events
+              .filter((key, value) -> value.getDuration() > 10)
+              .map((key, value) -> new KeyValue<>(value.getPage(), "0"))
+              .groupByKey()
+              .count(Materialized.as(AnalyticsBinding.PAGE_COUNT_MV))
+              .toStream();
+
+    }
+*/
+
+  @Slf4j
+  @Component
+  public static class PageViewEventProcessor {
+
+    @StreamListener
+    @SendTo(AnalyticsBinding.PAGE_COUNT_OUT)
+    public KStream<String, Long> process(@Input(AnalyticsBinding.PAGE_VIEWS_IN) KStream<String, PageViewEvent> events) {
+      return events
+              .filter((key, value) -> value.getDuration() > 10)
+              .map((key, value) -> new KeyValue<>(value.getPage(), "0"))
+              .groupByKey()
+              .count(Materialized.as(AnalyticsBinding.PAGE_COUNT_MV))
+              .toStream();
+    }
+  }
+
+
+  @Slf4j
+  @Component
+  public static class PageCountSink {
+    @StreamListener
+    public void process(@Input(AnalyticsBinding.PAGE_COUNT_IN) KTable<String, Long> counts) {
+      counts.toStream()
+              .foreach((key, value) -> log.info("{} = {}", key, value));
+    }
+  }
+
+  @RestController
+  @RequiredArgsConstructor
+  public static class CountRestController {
+
+    private final QueryableStoreProvider registry;
+
+    @GetMapping("/counts")
+    public Map<String, Long> counts() {
+      Map<String, Long> counts = new HashMap<>();
+      ReadOnlyKeyValueStore<String, Long> store = this.registry.getStore(AnalyticsBinding.PAGE_COUNT_MV, QueryableStoreTypes.keyValueStore());
+      KeyValueIterator<String, Long> all = store.all();
+      while (all.hasNext()) {
+        KeyValue<String, Long> value = all.next();
+        counts.put(value.key, value.value);
+      }
+      return counts;
+    }
+  }
 
 }
 
 
 interface AnalyticsBinding {
   String PAGE_VIEW_OUT = "pvout";
+  String PAGE_VIEWS_IN = "pvin";
+  String PAGE_COUNT_MV = "pcmv";
+  String PAGE_COUNT_OUT = "pcout";
+  String PAGE_COUNT_IN = "pcin";
 
   @Output
   MessageChannel pageViewOut();
+
+  @Input(PAGE_VIEWS_IN)
+  KStream<String, PageViewEvent> pageViewsIn();
+
+  @Output(PAGE_COUNT_OUT)
+  KStream<String, Long> pageCountOut();
+
+  @Input(PAGE_COUNT_IN)
+  KTable<String, Long> pageCountIn();
 }
 
 
