fb3527f6430e8d1287d92e7b75e8a072e9f8d8ec
PSK <wiv821253@gmail.com>
2020-05-25 20:32:46 +0900

    add first kafka app

--- src/main/java/com/psawesome/kafkacloudstreamexam/KafkaCloudStreamExamApplication.java
+++ src/main/java/com/psawesome/kafkacloudstreamexam/KafkaCloudStreamExamApplication.java
@@ -1,9 +1,34 @@
 package com.psawesome.kafkacloudstreamexam;
 
+import io.netty.util.concurrent.FastThreadLocalThread;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 @SpringBootApplication
+@EnableBinding(AnalyticsBinding.class)
 public class KafkaCloudStreamExamApplication {
 
 
@@ -28,4 +53,52 @@ public class KafkaCloudStreamExamApplication {
         SpringApplication.run(KafkaCloudStreamExamApplication.class, args);
     }
 
+
+    @Component
+    @RequiredArgsConstructor
+    public static class PageViewEventSource implements ApplicationRunner {
+        private final MessageChannel pageViewOut;
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            List<String> names = Arrays.asList("mfisher", "dyser", "schacko", "abinlan", "ozhurakousky", "grussell");
+            List<String> pages = Arrays.asList("blong", "sitemap", "initializr", "news", "colophon", "about");
+
+            Runnable runnable = () -> {
+                String rPage = pages.get(ThreadLocalRandom.current().nextInt(pages.size()));
+                String rName = pages.get(ThreadLocalRandom.current().nextInt(names.size()));
+
+                PageViewEvent pageViewEvent = new PageViewEvent(rName, rPage, Math.random() > .5 ? 10 : 1000);
+                Message<PageViewEvent> message = MessageBuilder
+                        .withPayload(pageViewEvent)
+                        .setHeader(KafkaHeaders.MESSAGE_KEY, pageViewEvent.getUserId().getBytes())
+                        .build();
+
+                try {
+                    this.pageViewOut.send(message);
+                    log.info("sent {}" , message.toString());
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                }
+            };
+            Executors.newScheduledThreadPool(1).scheduleAtFixedRate(runnable, 1, 1, TimeUnit.SECONDS);
+        }
+    }
+
+}
+
+
+interface AnalyticsBinding {
+    String PAGE_VIEW_OUT = "pvout";
+
+    @Output
+    MessageChannel pageViewOut();
+}
+
+
+@Data
+@AllArgsConstructor @NoArgsConstructor
+class PageViewEvent {
+    private String userId, page;
+    private long duration;
 }
